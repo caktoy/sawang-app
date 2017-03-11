3318fa17a4fe16af1921cb47076d908da8c19832
rifai <inoreds@gmail.com>
2017-03-12 01:07:55 +0700

    buat api & nambah file buat search

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('app');
 });
 
+Route::group(['prefix' => 'api/'], function() {
+    Route::resource('news', 'NewsController');
+});
+
 Route::get('/login', function() {
     return view('login');
 });
